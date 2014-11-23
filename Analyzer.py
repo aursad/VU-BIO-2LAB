a0131a67b3256f0df648e1cce1252a5564068fc4
Aurimas Sadauskas <aurimas.sadauskas@gmail.com>
2014-11-23 21:45:05 +0200

    mafft file analysis and search for exact and most diff seq

--- Analyzer.py
+++ Analyzer.py
@@ -78,15 +78,77 @@ class Analyzer:
         os.system('mafft --quiet ' + input + ' > ' + output)
         return
 
-def main():
-    part_length_to_analyze = 15
+    # Method to calculate mistakes in a part
+    def mistakes_in_part(self, main_sequence, all_seqs, start_index, part_length):
+        mistakes_count = 0;
 
+        # have to skip the first sequence because we compare with it
+        # for all sequences except first
+        for i in range(1, len(all_seqs)):
+            # take a sequence
+            sequence = all_seqs[i]
+            # count mistakes in main_sequence compared to that sequence that we took in a range
+            for j in range (start_index, start_index + part_length):
+                if (sequence[j] != main_sequence[j]):
+                    mistakes_count += 1
+        return mistakes_count
+
+    # Method to analyze sequence and find the most similar and dissimilar parts
+    # main_sequence to compare with must be first in a source file
+    def analysis(self, part_length, with_file = "mafft.fasta"):
+        # Reading source file to get sequences
+        all_seqs = []
+        for r in SeqIO.parse(open(with_file), "fasta"):
+            all_seqs.append(r.seq)
+            # NOTE: `source` file's first line should be the sequence that will be compared with others
+        # Let's mark main sequence
+        main_sequence = all_seqs[0]
+
+        # initialise markers for the most similar and dissimilar parts
+        min_mistakes = -1
+        min_index = 0
+        max_mistakes = -1
+        max_index  = 0
+
+        for i in range(0, len(main_sequence) - part_length):
+            # count mistakes starting from i to i+part_length
+            mistakes = self.mistakes_in_part(main_sequence, all_seqs, i, part_length)
+
+            # mark max and min mistakes in a part
+            if (mistakes > max_mistakes) or (min_mistakes == -1):
+                min_mistakes = mistakes
+                min_index = i
+            if (mistakes < min_mistakes) or (max_mistakes == -1):
+                max_mistakes = mistakes
+                max_index = i
+
+
+        print("Maziausiai panasi seka: " +\
+              str(min_index) + ".." + str(min_index + part_length) +\
+              " (klaidu: " + str(min_mistakes) + ")")
+
+        # Sequence checking
+        for sequence in all_seqs:
+            print(sequence[min_index:(min_index+part_length)])
+            break
+
+        print("Labiausiai panasi seka: " +\
+              str(max_index) + ".." + str(max_index + part_length) +\
+              " (klaidu: " + str(max_mistakes) + ")")
+        # Sequence checking
+        for sequence in all_seqs:
+            print(sequence[max_index:(max_index+part_length)])
+            break
+        return
+
+def main():
     seqAn = Analyzer("blastp", "swissprot", '"serum albumin"[Protein name] AND mammals[Organism]', "XML",  80);
     seqAn.loadMainAlbumin("serum_albumin_preproprotein.fasta");
 
-    blast = seqAn.blast();
-    seqAn.saveBlastRecordAsFasta(blast, "blast_output.fasta");
-    # seqAn.startMafft("blast_output.fasta");
+    #blast = seqAn.blast();
+    #seqAn.saveBlastRecordAsFasta(blast, "blast_output.fasta");
+    #seqAn.startMafft("blast_output.fasta");
+    seqAn.analysis(15);
 
 # Run main
 main()
